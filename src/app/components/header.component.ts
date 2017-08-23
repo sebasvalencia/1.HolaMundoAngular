8f31cd91b18ffe55627c45a0c8cd8888b56227ac
Sebastian Valencia <sebastian.valencian@gmail.com>
2017-08-22 19:48:43 -0500

    Se agrega el componente body

--- src/app/components/header.component.ts
+++ src/app/components/header.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector:'app-header',
-    template: `<h1>Este es el header component</h1>`
+    templateUrl: './header.component.html' 
 })
 export class HeaderComponent {
 
