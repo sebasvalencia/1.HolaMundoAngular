f8d014ceb47d79e8ac50fae3c0ec0b2c7f30f0c6
Sebastian Valencia <sebastian.valencian@gmail.com>
2017-08-22 20:01:18 -0500

    Se agrega el componente footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header.component';
 import { BodyComponent } from './components/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BodyComponent
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
